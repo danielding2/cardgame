94386c96694a6b90820d52784230e9f44752cec6
raysii3 <30005277+raysii3@users.noreply.github.com>
2018-04-10 18:27:00 +0800

    CardGame with GUI(Reuploaded)
    Imported the classes from cardgame(Console Version), and tweaked them to fit the GUI version based off VB WPF

---
+++ CardGame.GUI/CardGame.GUI/GameMechanism/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardGame.GUI.Players;
+using CardGame.GUI.Deck;
+
+namespace CardGame.GUI.GameMechanism
+{
+    public class GameMech
+    {
+        
+    }
+}
---
+++ CardGame.GUI/CardGame.GUI/General/Suits.cs
@@ -0,0 +1,11 @@
+
+namespace CardGame.GUI.General
+{
+    public enum Suits
+    {
+        SPADES = 0,
+        HEARTS,
+        DIAMONDS,
+        CLUBS
+    }
+}
