b00177f2ad061b1bb389199f6d6e98a4f74b73d4
raysii3 <30005277+raysii3@users.noreply.github.com>
2018-04-10 15:53:04 +0800

    Bare Version of CardGame with GUI Implementation
    Imported the classes from cardgame(Console Version), and tweaked them to fit the GUI version based off VB WPF

---
+++ CardGame.GUI/CardGame.GUI/General/Ranks.cs
@@ -0,0 +1,21 @@
+
+namespace CardGame.GUI.General
+{
+    public enum Ranks
+    {
+        ACE = 1,
+        DEUCE,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+
+    }
+}
